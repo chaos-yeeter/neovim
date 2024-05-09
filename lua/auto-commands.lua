2d3ba9a01d22236c3762e772f533275a1fe15b43
Mihir Kayastha <kayasthamihir11@gmail.com>
2024-05-09 08:39:45 +0530

    feat: Organize imports on save for python
    - When formatting a python file with ruff, include `I` flag to organize
      imports

--- lua/auto-commands.lua
+++ lua/auto-commands.lua
@@ -2,10 +2,10 @@ local format_autocmd_group = vim.api.nvim_create_augroup("AutoFormat", { clear =
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     pattern = { "*.py" },
-    desc = "Format python file after saving",
+    desc = "Format python file on save",
     callback = function()
         local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd(":silent !ruff check --fix " .. fileName)
+        vim.cmd(":silent !ruff check --fix --select I " .. fileName)
         vim.cmd(":silent !ruff format " .. fileName)
       end,
     group = format_autocmd_group,

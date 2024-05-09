fac491fab061493abb0b3ece51df7934611d6676
Mihir Kayastha <kayasthamihir11@gmail.com>
2024-05-09 08:35:12 +0530

    fix!: Disable treesitter for python
    - This is done to avoid glitched indenting in some cases
      e.g. When a string has a opening bracket or `:`, treesitter adds
      indent to the following line, which is incorrect

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,9 +1,10 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    ensure_installed = { "lua" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        disable = { "python" }, -- to avoid glitched indenting in python
+        additional_vim_regex_highlighting = false,
+    },
 }

489aa0acc24ca19a0acb356cf23b0b523fd8bb71
Mihir Kayastha <kayasthamihir11@gmail.com>
2024-06-11 19:57:31 +0530

    feat!: Split towards bottom-right
    - Vertical split to right instead of left
    - Horizontal split to bottom instead of top

--- lua/editor.lua
+++ lua/editor.lua
@@ -34,3 +34,7 @@ Utils.set("signcolumn", "yes")
 -- add j/k movements that are bigger than 1 line to jumplist
 Utils.map("n", "j", [[v:count ? (v:count > 1 ? "m'" . v:count : '') . 'j' : 'gj']], { expr = true })
 Utils.map("n", "k", [[v:count ? (v:count > 1 ? "m'" . v:count : '') . 'k' : 'gk']], { expr = true })
+
+-- split below for vertical & to right for horizontal
+Utils.set("splitbelow", true)
+Utils.set("splitright", true)

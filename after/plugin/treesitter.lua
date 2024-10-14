a1638cc74f109064b20fe19c486f953ebad77126
chaos-yeeter <kayasthamihir11@gmail.com>
2024-10-14 18:00:22 +0530

    feat(treesitter): Install python, bash, gitcommit & toml parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "lua" },
+    ensure_installed = { "lua", "python", "gitcommit", "toml", "bash" },
     sync_install = false,
     auto_install = true,
     highlight = {

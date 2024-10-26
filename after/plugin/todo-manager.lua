1ebec5ed881db269bde4bb0d5e04816dfd0626ce
chaos-yeeter <kayasthamihir11@gmail.com>
2024-10-26 22:20:31 +0530

    feat: Search & highlight TODOs
    - Install todo-comments.nvim
        - Ref: https://github.com/folke/todo-comments.nvim
    - Move between TODOs using `[t` and `]t`
    - Search TODOs using `<leader>ft`

---
+++ after/plugin/todo-manager.lua
@@ -0,0 +1,8 @@
+local Utils = require("utils")
+local todo_comments = require("todo-comments")
+
+todo_comments.setup {}
+
+Utils.map("n", "]t", require("todo-comments").jump_next, { desc = "Next todo comment" })
+Utils.map("n", "[t", require("todo-comments").jump_prev, { desc = "Previous todo comment" })
+Utils.map("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Search TODOs in the project" })

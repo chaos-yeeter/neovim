6359256b85b6dcf0674fc014ee82ac232a5aaa14
chaos-yeeter <kayasthamihir11@gmail.com>
2024-11-12 18:46:01 +0530

    feat: Setup auto reload
    - When files change outside neovim, reload buffer on:
        - Enter
        - Cursor hold
        - Focus gain

---
+++ after/plugin/auto-reload.lua
@@ -0,0 +1,6 @@
+Utils.set("autoread", true)
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+    pattern = "*",
+    command = "if mode() != 'c' | checktime | endif",
+})

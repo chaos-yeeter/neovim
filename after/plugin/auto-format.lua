028729a94cb286f1bd636c77c582496384c21505
chaos-yeeter <kayasthamihir11@gmail.com>
2024-11-17 11:47:56 +0530

    feat: Migrate to conform.nvim for formatting files on save
    - We use conform because it:
        - Preserves marks & folds
        - Provides range formatting for any formatter
        - Can format embedded code blocks (e.g. SQL in code or markdown
          blocks)
    - Setup format on save for:
        - Python using `ruff`
        - SQL using `sleek`
        - Web files (js/ts, jsx/tsx, md, etc.) using `prettierd`
        - Lua using `stylua`

--- after/plugin/auto-format.lua
+++ after/plugin/auto-format.lua
@@ -1,35 +1,19 @@
-local format_autocmd_group = vim.api.nvim_create_augroup("AutoFormat", { clear = true })
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.py" },
-    desc = "Format python files on save",
-    callback = function()
-        local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd(":!ruff check --fix-only --select ALL " .. fileName)
-        vim.lsp.buf.format {
-            async = false,
-            name = "ruff_lsp",
-        }
-    end,
-    group = format_autocmd_group,
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.tsx", "*.ts", "*.jsx", "*.js", "*.html", "*.css", "*.md", "*.yaml", "*.json" },
-    desc = "Format web files on save",
-    callback = function()
-        local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd("!biome format --write " .. fileName)
-    end,
-    group = format_autocmd_group,
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.sql" },
-    desc = "Format SQL files on save",
-    callback = function()
-        local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd("!sleek " .. fileName)
-    end,
-    group = format_autocmd_group,
+require("conform").setup({
+	formatters_by_ft = {
+		python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
+		sql = { "sleek" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		javascript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		json = { "prettierd" },
+		yaml = { "prettierd" },
+		html = { "prettierd" },
+		css = { "prettierd" },
+		markdown = { "prettierd" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })

23c7391f49527b98e884cad6a857cb918763302b
chaos-yeeter <kayasthamihir11@gmail.com>
2025-01-20 19:10:32 +0530

    feat(session): Save session every 10s
    - Create util to run functions in intervals

--- after/plugin/session.lua
+++ after/plugin/session.lua
@@ -9,27 +9,31 @@ local function exists(path)
 	return not err and success
 end
 
+local function create_session()
+	-- do not create session when neovim is called with args e.g. rebase, commit, etc.
+	if next(vim.fn.argv()) ~= nil then
+		return
+	end
+
+	-- create session directory if it does not exist
+	if not exists(session_dir) then
+		os.execute("mkdir -p " .. session_dir)
+	end
+
+	local current_working_dirname = string.gmatch(vim.fn.getcwd(), "[%d%a-_]+$")()
+	assert(current_working_dirname, string.format("Invalid current_working_dirname: '%s'", current_working_dirname))
+	local session_file_path = session_dir .. current_working_dirname .. ".vim"
+	vim.cmd("silent mksession! " .. session_file_path)
+end
+
+_ = Utils.set_interval(create_session, 10000)
+
 vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
 	pattern = { "*" },
 	desc = "Create session on exit",
 	nested = true,
 	once = true,
-	callback = function()
-		-- do not create session when neovim is called with args e.g. rebase, commit, etc.
-		print("In create session")
-		if next(vim.fn.argv()) ~= nil then
-			return
-		end
-
-		-- create session directory if it does not exist
-		if not exists(session_dir) then
-			os.execute("mkdir -p " .. session_dir)
-		end
-
-		local current_working_dirname = string.gmatch(vim.fn.getcwd(), "[%a-_]+$")()
-		local session_file_path = session_dir .. current_working_dirname .. ".vim"
-		vim.cmd("silent mksession! " .. session_file_path)
-	end,
+	callback = create_session,
 	group = session_autocmd_group,
 })
 
@@ -44,7 +48,8 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 			return
 		end
 
-		local current_working_dirname = string.gmatch(vim.fn.getcwd(), "[%a-_]+$")()
+		local current_working_dirname = string.gmatch(vim.fn.getcwd(), "[%d%a-_]+$")()
+		assert(current_working_dirname, string.format("Invalid current_working_dirname: '%s'", current_working_dirname))
 		local session_file_path = session_dir .. current_working_dirname .. ".vim"
 		if not exists(session_file_path) then
 			return
--- lua/utils.lua
+++ lua/utils.lua
@@ -15,4 +15,14 @@ function Utils.change_keymap(mode, new_keymap, old_keymap)
 	vim.api.nvim_set_keymap(mode, new_keymap, old_keymap, {})
 end
 
+-- periodically execute callback
+-- ref: https://neovim.io/doc/user/luvref.html#uv_timer_t
+function Utils.set_interval(callback, interval_in_ms)
+	local timer = vim.uv.new_timer()
+	timer:start(interval_in_ms, interval_in_ms, function()
+		vim.schedule(callback)
+	end)
+	return timer
+end
+
 return Utils

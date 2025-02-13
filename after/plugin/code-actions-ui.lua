6f62b19c42db0f2fd6b7a50f21822943cb1f1064
chaos-yeeter <kayasthamihir11@gmail.com>
2025-02-13 10:51:36 +0530

    ref!: Remove code action ui
    BREAKING: This is done as code actions aren't used as much. So code
    actions will no longer be displayed in a telescope window & they will
    show up in small dialogue box at the bottom

--- after/plugin/code-actions-ui.lua
+++
@@ -1,8 +0,0 @@
-require("telescope").setup({
-	extensions = {
-		["ui-jselect"] = {
-			require("telescope.themes").get_dropdown({}),
-		},
-	},
-})
-require("telescope").load_extension("ui-select")

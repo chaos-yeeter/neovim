c7b932af60510a442ecdce4f865a15a3f14c387b
chaos-yeeter <kayasthamihir11@gmail.com>
2025-02-21 18:39:04 +0530

    ref(statusline)!: Switch places for modules based on importance
    - Move filename to section 'b' so that it's not truncated in narrower
      windows
    - Move branch to section 'y' (right side)
    - Move diagnostic & diff indicators to section 'c'

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -4,14 +4,27 @@ return {
 	config = function()
 		require("lualine").setup({
 			sections = {
-				lualine_c = {
+				lualine_a = { "mode" },
+				lualine_b = {
 					{
 						"filename",
 						path = 1,
 					},
 				},
-				lualine_x = { "filetype" },
-				lualine_y = {},
+				lualine_c = { "diff", "diagnostics" },
+				lualine_x = { { "filetype", icon_only = true, padding = 0 } },
+				lualine_y = {
+					{
+						"branch",
+						fmt = function(branch_name)
+							if string.len(branch_name) > 25 then
+								return string.sub(branch_name, 0, 22) .. "..."
+							end
+							return branch_name
+						end,
+					},
+				},
+				lualine_z = { "location" },
 			},
 		})
 	end,

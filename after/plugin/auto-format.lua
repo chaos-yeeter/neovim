78cec4178d4779344878550dd05a62edad0eea5d
chaos-yeeter <kayasthamihir11@gmail.com>
2025-04-19 23:43:15 +0530

    feat(format): Setup auto-formatting for nix files using `alejandra`

--- after/plugin/auto-format.lua
+++ after/plugin/auto-format.lua
@@ -12,6 +12,7 @@ require("conform").setup({
 		html = { "prettierd" },
 		css = { "prettierd" },
 		markdown = { "prettierd" },
+		nix = { "alejandra" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
